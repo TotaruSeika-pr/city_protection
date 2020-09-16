84006e1f3471e36789dae56b8ade10b78a20212b
TotaruSeika-pr <62000140+TotaruSeika-pr@users.noreply.github.com>
2020-09-16 16:33:15 +0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,11 @@
+
+
+def print_map(playr_say, playr_say_i, NPS_say, NPS_say_i, map_houses):
+    print(NPS_say[NPS_say_i] + 'Your map:\n\n', map_houses[0], map_houses[1], '\n', map_houses[2], map_houses[3], '\n')
+
+
+def options(playr_say, playr_say_i, NPS_say, NPS_say_i):
+    print()
+
+def oops(playr_say, playr_say_i, NPS_say, NPS_say_i):
+    print(NPS_say[NPS_say_i] + 'Oosp, i dont know this comand.')
